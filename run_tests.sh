5a9ea538dcc2e49f6202bd25971eeaee66024608
Matthias Noback <matthiasnoback@gmail.com>
2018-04-13 14:07:27 +0200

    Add phpstan

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-vendor/bin/phpunit --testsuite unit -v \
-    && vendor/bin/phpunit --testsuite integration -v \
-    && vendor/bin/behat --suite acceptance -vvv
+set -e
+
+vendor/bin/phpstan analyse -l max src test
+vendor/bin/phpunit --testsuite unit -v
+vendor/bin/phpunit --testsuite integration -v
+vendor/bin/behat --suite acceptance -vvv
