a738d29fe745c38a58a7ca9f70417f32807d5b0e
Matthias Noback <matthiasnoback@gmail.com>
2020-12-09 11:33:30 +0100

    No discount means 0.0% discount

--- src/Domain/Model/SalesInvoice/Discount.php
+++ src/Domain/Model/SalesInvoice/Discount.php
@@ -5,29 +5,25 @@ namespace Domain\Model\SalesInvoice;
 
 final class Discount
 {
-    private ?float $discount;
+    private float $discount;
 
-    private function __construct(?float $discount)
+    private function __construct(float $discount)
     {
         $this->discount = $discount;
     }
 
-    public static function fromFloat(?float $discount): self
+    public static function fromFloat(float $discount): self
     {
         return new self($discount);
     }
 
     public function discountAmount(float $amount): float
     {
-        if ($this->discount === null) {
-            return 0.0;
-        }
-
         return round($amount * $this->discount / 100, 2);
     }
 
     public static function noDiscount(): self
     {
-        return new self(null);
+        return new self(0.0);
     }
 }
